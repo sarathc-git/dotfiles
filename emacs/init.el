65a7d84271b0e14a581aeb7e8213228a0d257ce1
Sarath Chandra K <sarath.kummamuru@gojek.com>
2022-08-30 23:05:46 +0530

    Made further changes

--- emacs/init.el
+++ emacs/init.el
@@ -17,7 +17,7 @@
 ;; (unless (package-installed-p 'use-package)
 ;;   (package-install 'use-package))
 ;; (require 'use-package)
-;; (setq use-package-always-ensure t)
+;;(setq use-package-always-ensure t)
 
 ;; straight.el
 (setq package-enable-at-startup nil)
@@ -89,13 +89,14 @@
 
 ;;;
 
+
 ;;; Using hydra for some convenient key bindings.
 (straight-use-package 'hydra)
 
 (defhydra hydra-zoom (:color red)
   "zoom"
-  ("g" text-scale-increase "in")
-  ("l" text-scale-decrease "out")
+  ("<up>" text-scale-increase "in")
+  ("<down>" text-scale-decrease "out")
   )
 (global-set-key (kbd "H-z") 'hydra-zoom/body)
 
@@ -106,10 +107,10 @@
   ("f" other-frame "other frame" :column "frames")
   ("n" make-frame "New frame")
   
-  ("s-<up>" windmove-up "window-up" :column "windows")
-  ("s-<right>" windmove-right "window-right" :column "windows")
-  ("s-<left>" windmove-left "window-left")
-  ("s-<down>" windmove-down "window-down")
+  ("s-<up>"     windmove-up    "window-up"    :column "windows")
+  ("s-<right>"  windmove-right "window-right")
+  ("s-<left>"   windmove-left  "window-left")
+  ("s-<down>"   windmove-down  "window-down")
 
   ("S-<up>" hydra-move-splitter-up "splitter up" :column "splitter")
   ("S-<down>" hydra-move-splitter-down "splitter down")
@@ -119,17 +120,25 @@
   ("|" (lambda ()
          (interactive)
          (split-window-right)
-         (windmove-right)) :column "Split Window")
+         (windmove-right)) "split right and move":column "Split Window")
   ("_" (lambda ()
          (interactive)
-         (split-window-below)
+         (split-window-below "split below and move")
          (windmove-down)))
-  ("v" split-window-right)
-  ("x" split-window-below)
+  ("v" split-window-right "split right and stay")
+  ("x" split-window-below "split right and stay")
 
   ("d" delete-window :column "Miscellaneous")
   )
 (global-set-key (kbd "H-w") 'hydra-frames/body)
+
+(straight-use-package 'use-package)
+
+(use-package which-key
+  :config
+  (setq which-key-idle-delay 1.0)
+;;  (which-key-setup-minibufer)
+  (which-key-mode))
 
 ;;; TODO : Need to add org mode hydra menu.
 
@@ -140,6 +149,8 @@
   "magit"  
   ("p" magit-push "magit-push" :column "actions")
   ("c" magit-commit "magit-commit" )
+  ("t" git-timemachine-toogle)
+  
   ("la" magit-log-all "magit-log-all --graph" :column "status")
   ("s" magit-status "magit-status")
   ("d" magit-diff "magit-diff")
@@ -149,4 +160,47 @@
 
 ;;; TODO Org - Roam, PDF Tools, Org Noter, Org Drill
 
+(use-package org
+  :ensure org
+  )
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+
+(custom-theme-set-faces
+   'user
+   '(org-block ((t (:inherit fixed-pitch))))
+   '(org-code ((t (:inherit (shadow fixed-pitch)))))
+   '(org-document-info ((t (:foreground "dark orange"))))
+   '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+   '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+   '(org-link ((t (:foreground "royal blue" :underline t))))
+   '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+   '(org-property-value ((t (:inherit fixed-pitch))) t)
+   '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+   '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
+   '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
+   '(org-verbatim ((t (:inherit (shadow fixed-pitch))))))
+
+(use-package emacsql-sqlite3
+  :straight t)
+
+(use-package org-roam
+  :straight t
+  :custom
+  (org-roam-directory "~/orgs/roam")
+  (org-roam-completion-everywhere t)
+  (org-roam-database-connector 'sqlite3)
+  (org-tags-column 10)
+  :bind (("H-n b" . org-roam-buffer-toggle)
+	 ("H-n f" . org-roam-node-find)
+	 ("H-n i" . org-roam-node-insert)
+	 ("H-n h" . org-id-get-create)
+	 ("H-n t" . org-roam-buffer-toggle)
+	 :map org-mode-map
+	 ("H-n c" . completion-at-point))
+
+  :config
+  (org-roam-setup))
+
 ;;; LSP rest client 
+
+
